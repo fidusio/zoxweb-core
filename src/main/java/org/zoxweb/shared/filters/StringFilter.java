7d9131de977ceb5425de581d9537abbad73ee04b
javaconsigliere <javaconsigliere@gmail.com>
2024-10-20 23:20:33 -0700

    Created string filer to be continued

---
+++ src/main/java/org/zoxweb/shared/filters/StringFilter.java
@@ -0,0 +1,44 @@
+package org.zoxweb.shared.filters;
+
+import org.zoxweb.shared.util.NVGenericMap;
+import org.zoxweb.shared.util.SharedStringUtil;
+import org.zoxweb.shared.util.SharedUtil;
+
+public class StringFilter extends
+    DataFilter<String, String, Class<String>>
+{
+    enum SFType
+    {
+        BETWEEN
+    }
+
+    private final NVGenericMap config;
+    public StringFilter(String id, NVGenericMap config)
+    {
+        super(String.class, id, config.getValue("name"), config.getValue("description"));
+
+        this.config = config;
+    }
+    /**
+     * @param input
+     * @return
+     */
+    @Override
+    public String decode(String input)
+    {
+        SFType type = SharedUtil.lookupEnum(config.getValue("type"), SFType.values());
+        String ret = input;
+        switch(type)
+        {
+            case BETWEEN:
+                ret = SharedStringUtil.valueAfterLeftToken(ret,  config.getValue("prefix"));
+                ret = SharedStringUtil.valueBeforeLeftToken(ret, config.getValue("postfix"));
+//                StringToken strToken = SharedStringUtil.valueBetween(ret, config.getValue("prefix"), config.getValue("postfix"), false);
+//                if (strToken != null)
+//                    ret = strToken.getToken();
+                break;
+        }
+
+        return ret;
+    }
+}
