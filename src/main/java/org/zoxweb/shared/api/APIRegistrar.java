8dd75ecfa0ee5f486da96cf683c57a4a3712c643
javaconsigliere <javaconsigliere@gmail.com>
2025-10-07 08:04:35 -0700

    Created API Registrar

---
+++ src/main/java/org/zoxweb/shared/api/APIRegistrar.java
@@ -0,0 +1,45 @@
+package org.zoxweb.shared.api;
+
+import org.zoxweb.shared.util.NVGenericMap;
+import org.zoxweb.shared.util.RegistrarMap;
+
+import java.util.LinkedHashMap;
+
+public class APIRegistrar
+        extends RegistrarMap<String, APIServiceProvider<?, ?>, APIRegistrar> {
+    public static final APIRegistrar SINGLETON = new APIRegistrar();
+    private volatile APIServiceProvider<?, ?> asDefault = null;
+
+    private APIRegistrar() {
+        super(new LinkedHashMap<>());
+    }
+
+    public synchronized APIRegistrar register(APIServiceProvider<?, ?> apiServiceProvider) {
+        register(apiServiceProvider.getName(), apiServiceProvider);
+        return this;
+    }
+
+    public <V> V getDefault() {
+        return (V) asDefault;
+    }
+
+    public <V> APIRegistrar setDefault(V asDefault) {
+        this.asDefault = (APIServiceProvider<?, ?>) asDefault;
+        return this;
+    }
+
+
+    public NVGenericMap stats(boolean detailed)
+    {
+
+        NVGenericMap ret = null;
+        if (asDefault != null)
+            ret =  asDefault.ping(detailed);
+
+        if(ret == null)
+            ret = new NVGenericMap();
+
+        ret.setName("api-registrar");
+        return ret;
+    }
+}
