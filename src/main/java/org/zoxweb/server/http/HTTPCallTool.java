84f2d41f019db4eba695f068579803c811af0e55
javaconsigliere <javaconsigliere@gmail.com>
2025-10-26 09:11:18 -0700

    code release 2.3.8

--- src/main/java/org/zoxweb/server/http/HTTPCallTool.java
+++ src/main/java/org/zoxweb/server/http/HTTPCallTool.java
@@ -75,7 +75,7 @@ public final class HTTPCallTool {
 
     public static void main(String... args) {
         try {
-            TaskUtil.setThreadMultiplier(96);
+            TaskUtil.setThreadMultiplier(11);
             TaskUtil.setMaxTasksQueue(2048);
             //System.setProperty("http.maxConnections", "100");
 
