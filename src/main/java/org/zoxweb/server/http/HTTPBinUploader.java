18a896589aca195eaa72c1eef0cbdf9698d61221
mnael <javaconsigliere@gmail.com>
2026-05-14 11:05:36 -0700

    Added support for parallel multi file update

--- src/main/java/org/zoxweb/server/http/HTTPBinUploader.java
+++ src/main/java/org/zoxweb/server/http/HTTPBinUploader.java
@@ -1,22 +1,26 @@
 package org.zoxweb.server.http;
 
+import org.zoxweb.server.logging.LogWrapper;
+import org.zoxweb.server.task.TaskUtil;
 import org.zoxweb.server.util.GSONUtil;
 import org.zoxweb.server.util.ServerUtil;
 import org.zoxweb.shared.http.*;
-import org.zoxweb.shared.util.NVEnum;
-import org.zoxweb.shared.util.NVGenericMap;
-import org.zoxweb.shared.util.NamedValue;
-import org.zoxweb.shared.util.ParamUtil;
+import org.zoxweb.shared.io.SharedIOUtil;
+import org.zoxweb.shared.util.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HTTPBinUploader {
+
+    private static final AtomicInteger completionCounter = new AtomicInteger(0);
+    public static final LogWrapper log = new LogWrapper(HTTPBinUploader.class).setEnabled(true);
     /**
      *
-     * @param url the upalod url
+     * @param url the upload url
      * @param username the user id
      * @param password the user password
      * @param filepath file to upload
@@ -39,22 +43,27 @@ public class HTTPBinUploader {
 
         HTTPMessageConfigInterface hmci = HTTPMessageConfig.createAndInit(url, null, HTTPMethod.POST, false);
         hmci.setBasicAuthorization(username, password);
-        FileInputStream fis = new FileInputStream(file);
-        hmci.setTransferChunked(true);
-        if (cmp) {
-            // content type MULTIPART-FORM-DATA
-            hmci.setContentType(HTTPMediaType.MULTIPART_FORM_DATA);
-            hmci.setBasicAuthorization(username, password);
-            NamedValue<InputStream> nv = new NamedValue<>("file", fis);
-            nv.getProperties().build(HTTPConst.CNP.FILENAME, file.getName())
-                    .build(new NVEnum(HTTPConst.CNP.MEDIA_TYPE, HTTPMediaType.APPLICATION_OCTET_STREAM));
-            hmci.getParameters().add(nv);
-        } else {
-            // content APPLICATION-OCTET-STREAM
-            hmci.setContentType(HTTPMediaType.APPLICATION_OCTET_STREAM);
-            hmci.setContentAsIS(fis, false);
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            hmci.setTransferChunked(true);
+            if (cmp) {
+                // content type MULTIPART-FORM-DATA
+                hmci.setContentType(HTTPMediaType.MULTIPART_FORM_DATA);
+                hmci.setBasicAuthorization(username, password);
+                NamedValue<InputStream> nv = new NamedValue<>("file", fis);
+                nv.getProperties().build(HTTPConst.CNP.FILENAME, file.getName())
+                        .build(new NVEnum(HTTPConst.CNP.MEDIA_TYPE, HTTPMediaType.APPLICATION_OCTET_STREAM));
+                hmci.getParameters().add(nv);
+            } else {
+                // content APPLICATION-OCTET-STREAM
+                hmci.setContentType(HTTPMediaType.APPLICATION_OCTET_STREAM);
+                hmci.setContentAsIS(fis, false);
+            }
+        } catch (IOException e) {
+            SharedIOUtil.close(fis);
+            throw e;
         }
-
         HTTPResponseData hrd = OkHTTPCall.send(hmci);
 
         return GSONUtil.fromJSONDefault(hrd.getData(), NVGenericMap.class);
@@ -130,27 +139,45 @@ public class HTTPBinUploader {
             String username = params.stringValue("user");
             String password = params.stringValue("password");
             String[] files = ParamUtil.parseWithSep(",", params.stringValue("files"));
+            boolean parallel = params.booleanValue("parallel", false);
             boolean cmp = params.booleanValue("cmp", true);
 
             System.out.println(params);
 
-
+            long ts = System.currentTimeMillis();
             for (String filepath : files) {
-                System.out.println("Trying to upload: " + filepath + " to: " + url);
-                try {
-                    NVGenericMap resp = uploadFile(url, username, password, filepath, cmp);
-                    System.out.println(GSONUtil.toJSONDefault(resp, true));
-                } catch (Exception e) {
-                    System.err.println(filepath + " failed to upload");
-                    e.printStackTrace();
+                if(parallel) {
+                    TaskUtil.defaultTaskProcessor().submit(()-> {
+                        log.getLogger().info("Trying to upload: " + filepath + " to: " + url);
+                        try {
+                            NVGenericMap resp = uploadFile(url, username, password, filepath, cmp);
+                            log.getLogger().info(GSONUtil.toJSONDefault(resp, true));
+                        } catch (Exception e) {
+                            System.err.println(filepath + " failed to upload");
+                            e.printStackTrace();
+                        }
+                        completionCounter.incrementAndGet();
+                    });
+                }
+                else {
+                    log.getLogger().info("Trying to upload: " + filepath + " to: " + url);
+                    try {
+                        NVGenericMap resp = uploadFile(url, username, password, filepath, cmp);
+                        log.getLogger().info(GSONUtil.toJSONDefault(resp, true));
+                    } catch (Exception e) {
+                        System.err.println(filepath + " failed to upload");
+                        e.printStackTrace();
+                    }
+                    completionCounter.incrementAndGet();
                 }
             }
-
-
+            TaskUtil.waitIfBusyThenClose(250, ()-> completionCounter.get() == files.length);
+            ts = System.currentTimeMillis() - ts;
+            log.getLogger().info("Overall it took: " + Const.TimeInMillis.toString(ts));
         } catch (Exception e) {
 
             e.printStackTrace();
-            ServerUtil.exitWithError(-1, "Usage: java HTTPBinUploader url=https://domain/upload user=username password=password file=file1,file2,... [cmp=true/false(chunked multipart form)]");
+            ServerUtil.exitWithError(-1, "Usage: java HTTPBinUploader url=https://domain/upload user=username password=password files=file1,file2,... [cmp=true/false(chunked multipart form)] [parallel=true/false(upload in parallel)]");
         }
     }
 
